6b0cf51d0fe3685e82ba0d9cb3543cc70733be6f
Tan Yeong Chai <butchercai@gmail.com>
2015-01-16 23:24:56 +0800

    Here's the initial version of Lab 1 Part 1, where LogCat is used to keep track the value of savedInstanceState within the onCreate() function.

---
+++ app/src/androidTest/java/sg/edu/sit/ict/lab1part1_logcat/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sg.edu.sit.ict.lab1part1_logcat;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
